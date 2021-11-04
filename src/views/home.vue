1a2cf7ab71edd1f05d82fd0ead54563e2767846f
王文浩 <3087808840@qq.com>
2021-11-04 17:23:04 +0800

    wwh完成

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,15 +1,17 @@
 <template>
-    <div>
-        啊哈哈哈哈
-    </div>
+  <div>
+    <wwh></wwh>
+  </div>
 </template>
 <script>
-
+import wwh from "@/views/wwh.vue";
 export default {
-
-}
+  name: "home",
+  components: {
+    wwh,
+  },
+};
 </script>
 
 <style lang='scss' scoped>
-
 </style>
